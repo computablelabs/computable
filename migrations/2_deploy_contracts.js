69b505e0cc1a883c0786f1a079ca02d228dfab7c
Bharath Ramsundar <rbharath@stanford.edu>
2018-02-12 17:00:27 -0800

    validator test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var DataCoin = artifacts.require("DataCoin");
+var Validator = artifacts.require("Validator");
 
 module.exports = function(deployer) {
   deployer.deploy(DataCoin);
+  deployer.deploy(Validator);
 }
---
+++ test/validator.js
@@ -0,0 +1,13 @@
+// Specifically request an abstraction for DataCoin
+var Validator = artifacts.require("Validator");
+
+contract('Validator', function(accounts) {
+  it("Should instantiate validator correctly", function () {
+    // TODO(rbharath): Seems to always deploy at account[0] even if we change to Validator.deployed(accounts[1]). Figure out what's going on.
+    return Validator.deployed(accounts[0]).then(function(instance) {
+      return instance.owner.call();
+    }).then(function(owner) {
+      assert.equal(owner, accounts[0], "Owner isn't creator of validator");
+    });
+  });
+});
