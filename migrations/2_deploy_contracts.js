f9cf38ebf55cb3a5514dfb96976277b863deed54
robrobbins <rob.robb.ns@gmail.com>
2018-04-19 17:47:39 -0700

    Skeletal tooling setup for TCR contracts, installing dependencies and compiling them

--- migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-var DataCoin = artifacts.require('DataCoin');
-var Validator = artifacts.require('Validator');
-var DataRegistry = artifacts.require('DataRegistry');
-
-module.exports = function (deployer) {
-  deployer.deploy(DataCoin);
-  deployer.deploy(Validator);
-  deployer.deploy(DataRegistry, 'TestRegistry');
-};
